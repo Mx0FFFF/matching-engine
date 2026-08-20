bd0857458c92fbc61e87d4ea1067c2826b1c8223
Mx0FFF <mx0ffffff@gmail.com>
2026-08-20 10:56:37 +0200

    Add trade record

---
+++ src/main/java/com/mx0ffff/engine/Trade.java
@@ -0,0 +1,12 @@
+package com.mx0ffff.engine;
+import java.time.Instant;
+
+public class Trade {
+    // A record of trades
+    private final String buyOrderId;
+    privat final String sellOrderId;
+    private final String symbol;
+    private final int quantity;
+    private final long price;
+    private final Instant timestamp;
+}
