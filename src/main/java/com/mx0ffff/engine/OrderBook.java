e62109ec0c21a50a7bbb74284ca16a6278f7f58e
Mx0FFF <mx0ffffff@gmail.com>
2026-08-22 18:30:18 +0200

    Implement bestBid and bestAsk

---
+++ src/main/java/com/mx0ffff/engine/OrderBook.java
@@ -0,0 +1,39 @@
+package com.mx0ffff.engine;
+
+import java.util.*;
+
+public class OrderBook {
+    private final NavigableMap<Long, Queue<Order>> bids;
+    private final NavigableMap<Long, Queue<Order>> asks;
+    private final String symbol;
+
+    public OrderBook (String symbol) {
+        this.symbol = symbol;
+        this.bids = new Treemap<>(Comparator.reverseOrder());
+        this.asks = new Treemap<>();
+    }
+
+    public List<Trade> submitOrder(Order order) {
+
+    }
+
+    public boolean cancelOrder(String orderId) {
+
+    }
+
+    public Optional<Long> bestBid() {
+        // if map = empty return emtpy Optional. else wrap bids.firstkey() in an optional
+        if (bids.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(bids.firstKey());
+    }
+
+    public Optional<Long> bestAsk() {
+        if (asks.isEmpty()) {
+            return Optional.empty();
+        }
+        return Optional.of(asks.firstKey());
+    }
+
+}
